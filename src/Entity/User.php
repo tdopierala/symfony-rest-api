ebed0e0d2f32d9df48b5c0c14cbea7a69a506158
Tomasz Dopierala <tomasz@dopierala.net.pl>
2021-01-27 13:38:27 +0100

    create user migration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,104 +12,158 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class User implements UserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
-    private $email;
+	/**
+	 * @ORM\Column(type="string", length=180, unique=true)
+	 */
+	private $email;
 
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
+	/**
+	 * @ORM\Column(type="json")
+	 */
+	private $roles = [];
 
-    /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
-     */
-    private $password;
+	/**
+	 * @var string The hashed password
+	 * @ORM\Column(type="string")
+	 */
+	private $password;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	/**
+	 * @ORM\Column(type="string", length=50, unique=true)
+	 */
+	private $token;
+	
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $created;
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $lastlogin;
+	
+	public function __construct()
+	{
+		$this->created = new \DateTime("now");
+		$this->lastlogin = new \DateTime("now");
+	}
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-        return $this;
-    }
+	public function getEmail(): ?string
+	{
+		return $this->email;
+	}
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUsername(): string
-    {
-        return (string) $this->email;
-    }
+	public function setEmail(string $email): self
+	{
+		$this->email = $email;
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+		return $this;
+	}
 
-        return array_unique($roles);
-    }
+	/**
+	 * A visual identifier that represents this user.
+	 *
+	 * @see UserInterface
+	 */
+	public function getUsername(): string
+	{
+		return (string) $this->email;
+	}
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
+	/**
+	 * @see UserInterface
+	 */
+	public function getRoles(): array
+	{
+		$roles = $this->roles;
+		// guarantee every user at least has ROLE_USER
+		$roles[] = 'ROLE_USER';
 
-        return $this;
-    }
+		return array_unique($roles);
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function getPassword(): string
-    {
-        return (string) $this->password;
-    }
+	public function setRoles(array $roles): self
+	{
+		$this->roles = $roles;
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function getPassword(): string
+	{
+		return (string) $this->password;
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function getSalt()
-    {
-        // not needed when using the "bcrypt" algorithm in security.yaml
-    }
+	public function setPassword(string $password): self
+	{
+		$this->password = $password;
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
+		return $this;
+	}
+
+	public function getToken(): ?string
+	{
+		return $this->token;
+	}
+
+	public function setToken(string $token): self
+	{
+		$this->token = $token;
+		return $this;
+	}
+	
+	public function getCreated(): ?\DateTimeInterface
+	{
+		return $this->created;
+	}
+
+	public function setCreated(\DateTimeInterface $created): self
+	{
+		$this->created = $created;
+		return $this;
+	}
+
+	public function getLastlogin(): ?\DateTimeInterface
+	{
+		return $this->lastlogin;
+	}
+
+	public function setLastlogin(\DateTimeInterface $lastlogin): self
+	{
+		$this->lastlogin = $lastlogin;
+		return $this;
+	}
+
+	/**
+	 * @see UserInterface
+	 */
+	public function getSalt()
+	{
+		// not needed when using the "bcrypt" algorithm in security.yaml
+	}
+
+	/**
+	 * @see UserInterface
+	 */
+	public function eraseCredentials()
+	{
+		// If you store any temporary, sensitive data on the user, clear it here
+		// $this->plainPassword = null;
+	}
 }
